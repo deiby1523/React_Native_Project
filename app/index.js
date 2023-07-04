3d1724ceef4266b8f0197c096e7cba4feb68c937
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-07-03 19:11:59 -0500

    Creation of Welcome component and search bar

--- app/index.js
+++ app/index.js
@@ -1,13 +1,49 @@
-import { Text,View } from "react-native";
-import { Link, Stack } from "expo-router";
+import { useState } from "react";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
-export default function Home() {
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter()
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      {/* Use the `Screen` component to configure the layout. */}
-      <Stack.Screen options={{ title: "Overview" }} />
-      {/* Use the `Link` component to enable optimized client-side routing. */}
-      <Text style={ {fontSize:60} }>Hello World! 💻</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome/>
+
+          <Popularjobs/>
+          <Nearbyjobs/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
+
+export default Home;
