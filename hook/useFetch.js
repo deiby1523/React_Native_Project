eda91c846e6a51be231757ed7c81249a28392dec
Deiby_P <98618493+deiby1523@users.noreply.github.com>
2023-07-06 13:19:55 -0500

    RapidAPI Key Fix exposed on GitHub.

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '62eee090fdmsh4a59131d8009eecp195b62jsnbe8e319ee56c',
+      "X-RapidAPI-Key": '',
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
